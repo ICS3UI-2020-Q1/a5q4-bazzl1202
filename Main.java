fb3dcab950d44376cba401c3738f822d70559834
bazzl1202 <71136617+bazzl1202@users.noreply.github.com>
2020-10-06 10:21:43 -0400

    Asks for investment amount, interest rate, years and calculates final balance

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Asks investment, interest rate and years they are investing for then calculates final balance.
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -9,7 +10,29 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user how much they want to invest
+    System.out.println("Please enter a starting balance");
+    double balance = input.nextDouble();
     
+    // ask the user their interest rate
+    System.out.println("Please enter your interest rate as a full percentage");
+    double interest = input.nextDouble();
+
+    // ask the user for how long they are investing
+    System.out.println("Please enter the number of years you will invest for");
+    int investment = input.nextInt(); 
+
+    // declare and initilaize counting variable
+    int count = 1;
+
+    while( count <= investment){
+      balance =((balance*interest)+ balance);
+      count = count + 1;
+    }
+   System.out.println(balance);
+  
   }
 }
